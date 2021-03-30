3de03cbb248db9cc4ae3f1a5ac8969031d230113
EC2 Default User <ec2-user@ip-172-31-8-70.ec2.internal>
2021-03-30 14:33:25 +0000

    [Add]bookのindexページの左側の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,31 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @user = User.find(current_user.id)
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
