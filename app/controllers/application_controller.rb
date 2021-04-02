3b3a7475585c744c30b8172fd3e97dca609ca94c
EC2 Default User <ec2-user@ip-172-31-8-70.ec2.internal>
2021-04-02 12:57:41 +0000

    [Add] validationの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protected
-  
+
   def configure_permitted_parameters
     # ↓ nameのみをkeysに入れるとemailでsignupができない現象が発生する。
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
