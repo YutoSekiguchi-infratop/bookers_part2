f31d89f170ccbf8c0122b225884c6470817dd23f
EC2 Default User <ec2-user@ip-172-31-8-70.ec2.internal>
2021-04-02 12:05:04 +0000

    [Add] userのeditとupdateの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
     @user = User.find(current_user.id)
@@ -12,30 +12,25 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def edit
+    @user = User.find(current_user.id)
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
   end
 
-  def destroy
-  end
-  
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
-  resources :books
-  resources :users
+  resources :books, only:[:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only:[:index, :show, :edit, :update]
 end
